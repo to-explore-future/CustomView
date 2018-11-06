5d28175ac0a4c636195bffde889a0d5b951a166d
869518570@qq.com <869518570@qq.com>
2018-11-06 14:56:53 +0800

    last commit

---
+++ customView/src/main/java/com/example/customview/activity/path/ECGActivity.java
@@ -0,0 +1,15 @@
+package com.example.customview.activity.path;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.customview.R;
+
+public class ECGActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecg);
+    }
+}
--- customView/src/main/java/com/example/customview/activity/path/PathActivity.java
+++ customView/src/main/java/com/example/customview/activity/path/PathActivity.java
@@ -15,12 +15,13 @@ public class PathActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path);
 
-        Button mEarthRotate = (Button) findViewById(R.id.btn_path_earth_revolve_around_the_sun);
-        Button mPathTest = (Button) findViewById(R.id.btn_path_knoledge_point_test);
-        Button mPathDemo = (Button) findViewById(R.id.btn_path_test_demo);
-        Button mPathEffectView = (Button) findViewById(R.id.btn_pathEffectView);
-        Button mWaveView = (Button) findViewById(R.id.btn_wave_view);
-        Button mPathLine = (Button) findViewById(R.id.btn_path_line);
+        Button mEarthRotate = findViewById(R.id.btn_path_earth_revolve_around_the_sun);
+        Button mPathTest = findViewById(R.id.btn_path_knoledge_point_test);
+        Button mPathDemo = findViewById(R.id.btn_path_test_demo);
+        Button mPathEffectView = findViewById(R.id.btn_pathEffectView);
+        Button mWaveView = findViewById(R.id.btn_wave_view);
+        Button mPathLine = findViewById(R.id.btn_path_line);
+        Button ECGView = findViewById(R.id.ecg_view);
 
 
         mPathEffectView.setOnClickListener(this);
@@ -29,6 +30,7 @@ public class PathActivity extends Activity implements View.OnClickListener {
         mPathDemo.setOnClickListener(this);
         mWaveView.setOnClickListener(this);
         mPathLine.setOnClickListener(this);
+        ECGView.setOnClickListener(this);
     }
 
     @Override
@@ -37,21 +39,30 @@ public class PathActivity extends Activity implements View.OnClickListener {
             case R.id.btn_path_knoledge_point_test:
                 startActivity(new Intent(this, PathExampleActivity.class));
                 break;
+
             case R.id.btn_path_test_demo:
                 startActivity(new Intent(this, PathDemoActivity.class));
                 break;
+
             case R.id.btn_path_earth_revolve_around_the_sun:
                 startActivity(new Intent(this, RevolveAroundSunActivity.class));
                 break;
+
             case R.id.btn_pathEffectView:
                 startActivity(new Intent(this, PathEffectActivity.class));
                 break;
+
             case R.id.btn_wave_view:
-                 startActivity(new Intent(this, WaveViewActivity.class));
-                 break;
+                startActivity(new Intent(this, WaveViewActivity.class));
+                break;
+
             case R.id.btn_path_line:
-                 startActivity(new Intent(this, PathLineActivity.class));
-                 break;
+                startActivity(new Intent(this, PathLineActivity.class));
+                break;
+
+            case R.id.ecg_view:
+                startActivity(new Intent(this, ECGActivity.class));
+                break;
         }
     }
 }
